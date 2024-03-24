04d4b7d44606a957a09fa021db24c71b147cd1df
0sei531 <oseikofiantwi531@gmail.com>
2024-03-24 14:08:30 -0700

    h

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,17 @@
-# Set up SSH client configuration
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  content => template('ssh_config.erb'),
+include stdlib
+
+
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => 'PasswordAuthentication',
 }
 
-# Template for SSH client configuration
-# /etc/ssh/ssh_config.erb
-# Standard SSH client configuration
-
-# Turn off password authentication
-<% if file_line['Turn off passwd auth'] == 'ensure' -%>
-PasswordAuthentication no
-<% end -%>
-
-# Specify identity file
-<% if file_line['Declare identity file'] == 'ensure' -%>
-IdentityFile ~/.ssh/school
-<% end -%>
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
 
