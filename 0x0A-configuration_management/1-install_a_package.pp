4aefdf0e16245ae734cea35cfc4900a61797f470
0sei531 <oseikofiantwi531@gmail.com>
2024-03-25 05:33:18 -0700

    c

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,7 @@
 
-#Install flask version 2.1.0
-
-
-exec {'pip3 install flask':
-require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
-}
-
-exec {'python-installed':
-command => '/usr/bin/which python3'
+# Install puppet-lint --version 2.1.1.
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
 
